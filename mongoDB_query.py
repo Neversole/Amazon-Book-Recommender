9948a02924652b2f93ee6c48d84f4b74f6f0fb00
Natalie Eversole <71839994+Neversole@users.noreply.github.com>
2023-06-21 17:57:42 -0700

    Add files via upload

---
+++ mongoDB_query.py
@@ -0,0 +1,33 @@
+#######################################################
+# Authors: Natalie Eversole and Team members
+# Date: 12/08/2022
+# Description: This code creates our mongoDB database
+#######################################################
+
+import pandas as pd
+import json
+from pymongo import MongoClient
+import certifi
+# import dnspython
+
+def get_database():
+    from pymongo import MongoClient
+    import pymongo
+
+    CONNECTION_STRING = "mongodb+srv://wsu_cpts_415:2022CPTS415@cluster0.owwy0gt.mongodb.net/?retryWrites=true&w=majority"
+    client = MongoClient(CONNECTION_STRING,
+                         tlsCAFile=certifi.where())
+
+    mydatabase = client.CPTS415_PROJECT
+    collections = mydatabase.list_collection_names()
+    mycollections = mydatabase.JSON
+
+    print("List of collections", collections)
+
+    # Create the database for our example
+    return client['CPTS415_PROJECT']
+
+
+if __name__ == "__main__":
+    # Get the database
+    dbname = get_database()
